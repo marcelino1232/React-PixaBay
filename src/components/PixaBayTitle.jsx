90837624077166e007133269ce81677f5bedad79
Marcelino Heredia Fernandez <marcelinoheredia.02@gmail.com>
2026-09-22 23:04:01 -0400

    fix title

--- src/components/PixaBayTitle.jsx
+++ src/components/PixaBayTitle.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function PixaBayTitle() {
   return (
-    <div className='bg-danger text-center py-3'>
-      <h1 className='lead display-4 text-white mb-0'>Search Images - PIXABAY</h1>
+    <div className='bg-danger text-center py-2'>
+      <h1 className='lead display-5 text-white mb-0'>Search Images - PIXABAY</h1>
     </div>
   )
 }
