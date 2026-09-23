ec14eedce5f1f9d1baf896c41a1386f69ced9b57
Marcelino Heredia Fernandez <marcelinoheredia.02@gmail.com>
2026-09-22 22:15:38 -0400

    completed app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {useRef, useState } from 'react'
+import {useEffect, useRef, useState } from 'react'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
@@ -6,7 +6,6 @@ import './App.css'
 
 import SearchForm from './components/SearchForm';
 import Loading from './components/Loading';
-import EmptyList from './components/EmptyList';
 import ImageList from './components/ImageList';
 import PixaBayTitle from './components/PixaBayTitle';
 import Pagination from './components/Pagination';
@@ -21,41 +20,45 @@ function App() {
 
   const [data, setData] = useState({
     loading:true,
+    error:false,
     total:0,
     totalHits:0,
     hits:[]
   });
 
-  async function onloadEvent(search, page)
-  {
-    if(params.current.search.length > 0){
-      setData(prev => ({...prev,loading:false}));
-      const request = await fetch(`https://pixabay.com/api/?key=${import.meta.env.VITE_PIXABAY_KEY}&q=${search}&per_page=${params.current.columnRows}&page=${page}`);
-      const response = await request.json();
-      setData({...response,loading:true});
+  async function onloadEvent(search,page){
+    setData(prev => ({...prev,loading:false}));
+    const request = await fetch(`https://pixabay.com/api/?key=${import.meta.env.VITE_PIXABAY_KEY}&q=${search}&per_page=${params.current.columnRows}&page=${page}`);
+    const response = await request.json();
+    setData({...response,loading:true,error:response.hits.length <= 0});
+    
+  }
+
+  useEffect(() => {
+   if(data.error){
+    const timer = setTimeout(() => setData(prev => ({...prev,error:false})),1000);
+
+    // 2. Return a cleanup function to clear the timeout
+    return () => clearTimeout(timer);
+   }
+  },[data.error]);
+
+  const onsubmitHandler = async(e) => {
+    e.preventDefault();
+    if(e.target.checkValidity())
+    {
+      const formData = new FormData(e.currentTarget);
+
+      params.current.search = formData.get("search");
+      params.current.page = 1;
+
+      e.target.reset();
+      e.target.classList.remove("was-validated");
+
+      await onloadEvent(params.current.search,params.current.page);
     }else{
-      setData({
-        loading:true,
-        total:0,
-        totalHits:0,
-        hits:[]
-      });
+      e.target.classList.add('was-validated')
     }
-  }
-
-  const onsubmitHandler = async(e) =>{
-    e.preventDefault();
-    params.current.page = 1;
-    await onloadEvent(
-      params.current.search,
-      params.current.page
-    );
-  }
-
-  const onsearchEvent = (e) => {
-    e.preventDefault();
-    const {name,value} = e.target;
-    params.current = {...params.current,[name]:value};
   }
 
   const onpageEvent = async(e,status = true) => {
@@ -74,14 +77,15 @@ function App() {
   return (
     <>
     <PixaBayTitle />
-
-    <SearchForm onsearchEvent={onsearchEvent} onsubmitHandler={onsubmitHandler} />
+    
+    {data.error && (<p className="alert alert-danger border border-0 rounded-0 text-center h4">Sorry but the image was not found...</p>)}
+    
+    <SearchForm  onsubmitHandler={onsubmitHandler} />
     
     <div className='container-fluid my-5'>
       <div className='row g-4'>
         {!data.loading ? (<Loading />) : 
-          (data.hits.length > 0 ? 
-          (<ImageList data={data} />) : (<EmptyList />))
+          (data.hits.length > 0 && (<ImageList data={data} />))
         }
       </div>
     </div>
--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
 
-export default function SearchForm({onsubmitHandler,onsearchEvent}) {
+export default function SearchForm({onsubmitHandler}) {
   return (
     <div className='container-lg'>
-      <form className='row bg-img g-3' onSubmit={onsubmitHandler}>
-        <div className='form-floating col-12 col-md-9'>
+      <form noValidate className='row bg-img g-3 needs-validation' onSubmit={onsubmitHandler} >
+        <div className='form-floating  position-relative col-12 col-md-9'>
           <input 
           id="txtsearch" 
           type="text" 
           className='form-control form-control-lg' 
           placeholder='Image Name...'
           name='search'
-          onChange={onsearchEvent}
-          title='only letters'
-          pattern='[A-Za-z]+'
           required
           />
           <label htmlFor='txtsearch' className='form-label'>Image Name...</label>
+          <div className="invalid-feedback position">
+            Please provide a image name.
+          </div>
         </div>
-         <div className='col-12 col-md-3 d-grid'>
+         <div className='col-12 col-md-3 d-grid '>
           <button type='submit' className='btn btn-outline-danger'>Search</button>
          </div>
       </form>
